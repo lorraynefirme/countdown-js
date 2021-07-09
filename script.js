e402dab03e40d66cb9408a12ceed43d5d71afd8f
Lorrayne Firme <44826803+lorraynefirme@users.noreply.github.com>
2021-07-09 10:37:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+var dateControl = document.querySelector('input[type="date"]');
+
+const dateToday = new Date();
+const day = dateToday.getDate(); 
+const month = dateToday.getMonth();
+const year = dateToday.getFullYear();
+
+dateControl.value = `${year}-0${month+1}-0${day}`;
+
+const dayDiv = document.querySelector('#days');
+const hourDiv = document.querySelector('#hours');
+const minuteDiv = document.querySelector('#minutes');
+const secondDiv = document.querySelector('#seconds');
+
+
+class Countdown {
+  constructor(futureDate) {
+    this.futureDate = futureDate;
+  }
+  get _actualDate() {
+    return new Date();
+  }
+  get _futureDate() {
+    return new Date(this.futureDate);
+  }
+  get _timeStampDiff() {
+    return this._futureDate.getTime() - this._actualDate.getTime();
+  }
+  get days() {
+    return Math.floor(this._timeStampDiff / (24 * 60 * 60 * 1000));
+  }
+  get hours() {
+    return Math.floor(this._timeStampDiff / (60 * 60 * 1000));
+  }
+  get minutes() {
+    return Math.floor(this._timeStampDiff / (60 * 1000));
+  }
+  get seconds() {
+    return Math.floor(this._timeStampDiff / 1000);
+  }
+  get total() {
+    const days = this.days;
+    const hours = this.hours % 24;
+    const minutes = this.minutes % 60;
+    const seconds = this.seconds % 60;
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    }
+  }
+}
+
+
+dateControl.addEventListener('keydown', event =>{
+
+  if(event.key==='Enter'){
+    clearInterval(timer);
+
+    const date = event.target.value.split('-');
+    const dayChoose = date[2];
+    const yearChoose = date[0];
+    let monthChoose = '';
+  
+    if(date[1] === '01')
+      monthChoose = 'January';
+    else if(date[1] === '02')
+      monthChoose = 'February';
+    else if(date[1] === '03')
+      monthChoose = 'March';  
+    else if(date[1] === '04')
+      monthChoose = 'April'; 
+    else if(date[1] === '05')
+      monthChoose = 'May'; 
+    else if(date[1] === '06')
+      monthChoose = 'June'; 
+    else if(date[1] === '07')
+      monthChoose = 'July'; 
+    else if(date[1] === '08')
+      monthChoose = 'August';
+    else if(date[1] === '09')
+      monthChoose = 'September';  
+    else if(date[1] === '10')
+      monthChoose = 'October'; 
+    else if(date[1] === '11')
+      monthChoose = 'November'; 
+    else if(date[1] === '12')
+      monthChoose = 'December';
+
+    //console.log(day) number
+    //console.log(dayChoose) string
+
+    if(!(day == dayChoose && (month+1) == date[1] && year == yearChoose))
+      showDays(`${dayChoose} ${monthChoose} ${yearChoose} 23:59:59 GMT-0300`)
+
+    else{
+      dayDiv.innerText = 0;
+      hourDiv.innerText = 0;
+      minuteDiv.innerText = 0;
+      secondDiv.innerText = 0;
+    }
+
+  }
+
+})
+
+let timer;
+
+function showDays(date){
+
+  const howDays = new Countdown(date);
+
+  timer = setInterval(()=>{
+    dayDiv.innerText = howDays.total.days -1;
+    hourDiv.innerText = howDays.total.hours;
+    minuteDiv.innerText = howDays.total.minutes;
+    secondDiv.innerText = howDays.total.seconds;
+  }, 1000)
+
+}
